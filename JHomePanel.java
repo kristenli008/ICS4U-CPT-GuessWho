eac1c593a128d5165c82ac5083e1b8abc30eac31
Nicole <143809859+nicole0625@users.noreply.github.com>
2026-05-23 18:42:41 -0400

    Add files via upload

---
+++ JHomePanel.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.*;
+
+public class JHomePanel extends JPanel{
+	//Properties
+	
+	//Methods
+	public void paintComponent(Graphics g){
+		
+	}
+	
+	//Constructor
+	public JHomePanel(){
+		super();
+	}
+}
