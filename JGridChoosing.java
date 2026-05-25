86f5061b1be5b2c7b1cf8b8e83feb4501f3945a8
Nicole <143809859+nicole0625@users.noreply.github.com>
2026-05-25 14:01:11 -0400

    Grid Choosing Screen

---
+++ JGridChoosing.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.*;
+
+public class JGridChoosing extends JPanel{
+	//Propeties
+	
+	//Methods
+	public void paintComponent(Graphics g){
+	
+	}
+	
+	//Constructor
+	public JGridChoosing(){
+		super();
+	}
+	
+}
