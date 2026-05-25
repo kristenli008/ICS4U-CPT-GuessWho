e5c23962daa28dbc49ca58d57b8fe57486f2505a
kristenli008 <kristen.li26@ycdsbk12.ca>
2026-05-25 14:18:33 -0400

    checking network messages
    wrote code for checking type of network message, parsing network typing from message

--- SocketNetwork.java
+++ SocketNetwork.java
@@ -1,6 +1,24 @@
+import java.lang.*;
+
 public class SocketNetwork{
-	public int sendingcheck(){
-		
-		
+	public static int sendingcheck(String strMessage){
+		if(strMessage.startsWith("chat/")){
+			return 1;
+		}else if(strMessage.startsWith("turn/")){
+			return 2;
+		}else if(strMessage.startsWith("grid/")){
+			return 3;
+		}else if(strMessage.startsWith("answ/")){
+			return 4;
+		}else if(strMessage.startsWith("wilo/")){
+			return 5;
+		}else{
+			return 0;
+		}
+	}
+	
+	public static String parsemsg(String strMessage){
+		int intLength = strMessage.length();
+		return strMessage.substring(5,intLength);
 	}
 }
