a12fb936425a0a922d97e86399356db68d43557e
MChuiSTA <marcus.chui26@ycdsbk12.ca>
2026-06-10 13:09:43 -0400

    Reupdated comments

--- SocketNetwork.java
+++ SocketNetwork.java
@@ -9,38 +9,38 @@ public class SocketNetwork{
 	
 	/**Method for reading SuperSocketMaster messages */
 	public static int sendingcheck(String strMessage){
-		/**index for chat messages*/
+		// index for chat messages
 		if(strMessage.startsWith("chat/")){
 			return 1;
 			
-		/**index for asking questions*/
+		// index for asking questions
 		}else if(strMessage.startsWith("ques/")){
 			return 2;
 			
-		/**index for passing grid types*/
+		// index for passing grid types
 		}else if(strMessage.startsWith("grid/")){
 			return 3;
 			
-		/**index for passing question answers*/
+		// index for passing question answers
 		}else if(strMessage.startsWith("answ/")){
 			return 4;
 			
-		/**index for player winning / losing*/
+		// index for player winning / losing
 		}else if(strMessage.startsWith("wilo/")){
 			return 5;
 			
-		/**index for testing connection between player/host*/
+		// index for testing connection between player/host
 		}else if(strMessage.startsWith("test/")){
 			return 6;
 			
-		/**index for passing player / host ready to play*/
+		// index for passing player / host ready to play
 		}else if(strMessage.startsWith("redy/")){
 			return 7;
 			
-		/**index for guessing characters*/
+		// index for guessing characters
 		}else if(strMessage.startsWith("gues/")){
 			return 8;
-		/**error index*/
+		// error index
 		}else{
 			return 0;
 		}
